ddda0ce8a14cdb65061ede5e7416c8c2a4fd772d
Joel Hernandez <involvmnt@gmail.com>
2015-11-26 11:00:48 +0100

    Adds tests

--- test/promise-ensurer-spec.js
+++ test/promise-ensurer-spec.js
@@ -1,8 +1,75 @@
-var Greeter = require("../lib/greeter");
+var Promise = require('bluebird');
 
-describe("greeter", function () {
-  it("should greet with message", function () {
-    var greeter = new Greeter('friend');
-    expect(greeter.greet()).toBe('Bonjour, friend!');
-  });
+Promise.config({
+    warnings : false
+});
+
+var PromiseEnsurer = require('../lib/promise-ensurer');
+
+describe('Promise ensurer', function () {
+
+    describe('when receiving an undefined value', function () {
+
+        it('should return a promise', function () {
+            // Catching it to remove warning log
+            var result = PromiseEnsurer.ensure().catch(function () {});
+            expect(result instanceof Promise).toBe(true);
+        });
+
+        it('should return a rejected promise', function (done) {
+            PromiseEnsurer.ensure(undefined).then(function(){
+                done.fail();
+            }).catch(function () {
+                done();
+            });
+        });
+    });
+
+    describe('when receiving an object', function () {
+
+        var jedi;
+
+        beforeEach(function () {
+            jedi = {
+                name : 'Luke Skywalker'
+            };
+        });
+
+        it('should return a promise', function () {
+            var result = PromiseEnsurer.ensure(jedi);
+            expect(result instanceof Promise).toBe(true);
+        });
+
+        it('should return a resolved promise with the given object', function (done) {
+            PromiseEnsurer.ensure(jedi).then(function(returnedJedi){
+                expect(jedi).toEqual(returnedJedi);
+            }).catch(function(){
+                done.fail();
+            }).finally(function () {
+                done();
+            })
+        });
+    });
+
+    describe('when receiving a boolean value', function () {
+
+        it('should return a promise', function () {
+            var result = PromiseEnsurer.ensure(true);
+            expect(result instanceof Promise).toBe(true);
+        });
+
+        it('should return a resolved promise if it\'s true', function (done) {
+            PromiseEnsurer.ensure(true).then(function(){
+                done();
+            });
+        });
+
+        it('should return a rejected promise if it\'s false', function (done) {
+            PromiseEnsurer.ensure(false).then(function(){
+                done.fail();
+            }).catch(function () {
+                done();
+            });
+        })
+    });
 });
